9555363d07e5c1658c048de0265775584a71ffc2
Ben Weinstein <bweinstein@stuy.edu>
2017-02-11 21:58:53 -0500

    solve() is done, plus testing to nail 2x2, 3x3 cases (v2)

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -15,13 +15,14 @@ public class QueenBoard{
      *after having added all n queens. 
      *Uses solveH
      */
-    public boolean solve(){
-	return solveH(0);
+    public void solve(){
+	solveH(0);
     }
 
     private boolean solveH(int col){
 	for(int row = 0; row < board.length; row++){
 	    //base case for end of the board
+	    //NEEDS TO ACCOUNT FOR CHECKING AMOUNT OF QUEENS??? idts anymore
 	    if(col >= board.length){
 		return true;
 	    }
@@ -35,6 +36,11 @@ public class QueenBoard{
 		    deleteQueenHere(row,col);
 		}
 	    }
+
+	    //to account for 2x2, 3x3 boards
+	    if(row == board.length - 1 && (board.length == 2 || board.length == 3)){
+		return solveH(col + 1);
+	    }
 	}
 	return false;
     }
@@ -43,13 +49,78 @@ public class QueenBoard{
     //private boolean isQueenable(int row, int col){}
     //have to check diagonals, row, and col
     private boolean isQueenable(int row, int col){
-	return safeRow(row) && safeCol(col) && safeDiagonals(row, col);
+	return isSafeRow(row, col) && isSafeCol(row, col) && areSafeDiagonals(row, col);
     }
+    
     //the helper's helpers:
-    //private boolean safeRow(int row){}
-    //private boolean safeCol(int col){}
-    //private boolean safeDiagonals(int row, int col){}
+    private boolean isSafeRow(int row, int col){
+	for(int c = 0; c < board.length; c++){
+	    if(c != col && board[row][c] == -1){
+		return false;
+	    }
+	}
+	return true;
+    }
+    
+    private boolean isSafeCol(int row, int col){
+	for(int r = 0; r < board.length; r++){
+	    if(r != row && board[r][col] == -1){
+		return false;
+	    }
+	}
+	return true;
+    }
+    
+    private boolean areSafeDiagonals(int row, int col){
+	int r1 = row;
+	int r2 = row;
+	int r3 = row;
+	int r4 = row;
+	int c1 = col;
+	int c2 = col;
+	int c3 = col;
+	int c4 = col;
 
+	//down to the right
+	while(r1 < board.length && c1 < board.length){
+	    if(board[r1][c1] == -1){
+		return false;
+	    }
+	    r1++;
+	    c1++;
+	}
+
+	//up to the left
+	while(r2 >= 0 && c2 >= 0){
+	    if(board[r2][c2] == -1){
+		return false;
+	    }
+	    r2--;
+	    c2--;
+	}
+
+	//down to the left
+	while(r3 < board.length && c3 >= 0){
+	    if(board[r3][c3] == -1){
+		return false;
+	    }
+	    r3++;
+	    c3--;
+	}
+
+	//up to the right
+	while(r4 >= 0 && c4 > board.length){
+	    if(board[r4][c4] == -1){
+		return false;
+	    }
+	    r4--;
+	    c4++;
+	}
+	
+	//if it all works...
+	return true;
+    }	
+    
     //private void placeQueenHere(int row, int col){}
     private void placeQueenHere(int row, int col){
 	board[row][col] = -1;
@@ -66,8 +137,8 @@ public class QueenBoard{
      *updates solutionCount instance variable accordingly. 
      *Uses countH
      */
-    public boolean countSolutions(){
-	return countH();
+    public void countSolutions(){
+        countH();
     }
     
     private boolean countH(){
@@ -78,7 +149,7 @@ public class QueenBoard{
      *@return the number of solutions found, or -1 if the board was never solved.
      *The board should be reset after this is run.
      */
-    public int getSolutionCount(){
+    public int getCount(){
 	//blank the board
 	for(int r = 0; r < board.length; r++){
 	    for(int c = 0; c < board.length; c++){
@@ -98,11 +169,28 @@ public class QueenBoard{
      *all others are replaced with underscores ('_')
      */
     public String toString(){
-	return "";
+	String ans = "";
+	for(int row = 0; row < board.length; row++){
+	    for(int col = 0; col < board.length; col++){
+		if(board[row][col] == -1){
+		    ans += "Q ";
+		}
+		else{
+		    ans += "_ ";
+		}
+		if(col == board.length - 1){
+		    ans += "\n";
+		}
+	    }
+	}
+	return ans;	    
     }
 
     //TESTS
     public static void main(String[] args){
-	System.out.println("Hi");
+	QueenBoard n = new QueenBoard(4);
+	System.out.println(n);
+	n.solve();
+	System.out.println(n);
     }
 }
