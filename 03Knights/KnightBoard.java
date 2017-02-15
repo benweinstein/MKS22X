531c60441ccc74f2b4d3e8b13ffe419deac540e7
Ben Weinstein <bweinstein@stuy.edu>
2017-02-14 22:49:54 -0500

    code frame (v0)

---
+++ 03Knights/KnightBoard.java
@@ -0,0 +1,23 @@
+public class KnightBoard{
+    private int[][] board;
+
+    public KnightBoard(int startingRows, int startingCols){
+	board = new int[startingRows][startingCols];
+    }
+
+    public void solve(){
+	solveH(0, 0, 1);
+    }
+
+    private boolean solveH(int row, int col, int numKnight){
+
+
+	return false;
+    }
+    
+    public String toString(){
+	String ans = "";
+
+	return ans;
+    }
+}
