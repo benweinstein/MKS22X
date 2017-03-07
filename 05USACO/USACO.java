30e3ae7ed683db02af94c81e6858524b137f6b46
Ben Weinstein <bweinstein@stuy.edu>
2017-03-06 19:17:58 -0500

    created folder, code frame (v0)

---
+++ 05USACO/USACO.java
@@ -0,0 +1,19 @@
+public class USACO{
+    //BRONZE PROBLEM #12: LAKE MAKING
+    public int bronze(String filename){
+	return -1; //to be filled in later
+    }
+
+
+    //SILVER PROBLEM #7: COW TRAVELLING
+    public int silver(String filename){
+
+
+	return -1; //to be filled in later
+    }
+
+    //TESTS
+    public static void main(String[] args){
+	System.out.println("Hi");
+    }
+}
