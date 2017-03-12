8e37d373964764fdd9eedac0a2c1c67fa1812bc9
Ben Weinstein <bweinstein@stuy.edu>
2017-03-12 14:59:56 -0400

    put Silver.java into USACO.java (v8)

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -113,10 +113,128 @@ public class USACO{
     private File silverFile;
     private Scanner silverScan;
 
+    private int[][] pasture1;
+    private int[][] pasture2;
+    
+    private int goalR;
+    private int goalC;
+
+    //BETTER METHOD -- IDEA FROM CLASS ON FRIDAY, 3/10/17
+     public int silver(String filename){
+	try{
+	    silverFile = new File(filename);
+	    silverScan = new Scanner(silverFile);
+	}catch(FileNotFoundException e){
+	    System.out.println("file not found");
+	    System.exit(1);
+	}
+
+	int row = silverScan.nextInt();
+	int col = silverScan.nextInt();
+
+	pasture1 = new int[row][col];
+	pasture2 = new int[row][col];
+
+	//total number of seconds cow should take to get to new place in pasture
+	int seconds = silverScan.nextInt();
+
+	for(int r = 0; r < row; r++){
+	    String line = silverScan.next();
+	    for(int c = 0; c < col; c++){
+		char spot = line.charAt(c);
+		if(spot == '*'){
+		    pasture1[r][c] = -1;
+		    pasture2[r][c] = -1;
+		}//everything else stays at 0
+	    }
+	}
+	//printPastures();
+	
+	int r1 = silverScan.nextInt() - 1;
+	int c1 = silverScan.nextInt() - 1;
+	goalR = silverScan.nextInt() - 1;
+	goalC = silverScan.nextInt() - 1;
+	
+	try{
+	    pasture1[r1][c1] = 1;
+	}catch(ArrayIndexOutOfBoundsException e){
+	    System.out.println("invalid R1, C1");
+	    System.exit(1);
+	}
+	
+	return navigateH(pasture1, pasture2, seconds);  
+    }
+
+    //recursive helper for silver
+    private int navigateH(int[][] oldAry, int[][] newAry, int seconds){
+	//base case
+	if(seconds == 0){
+	    return oldAry[goalR][goalC];
+	}
+
+	//updating newAry
+	
+	for(int r = 0; r < newAry.length; r++){
+	    for(int c = 0; c < newAry[r].length; c++){
+		if(oldAry[r][c] != -1){ //NEEDED THIS!!! So that the -1's don't
+		    //ever get changed!!!
+		    int newVal = 0;
+		    try{
+			if(oldAry[r + 1][c] != -1){
+			    newVal += oldAry[r + 1][c];
+			}
+		    }catch(ArrayIndexOutOfBoundsException e){}
+		    
+		    try{
+			if(oldAry[r - 1][c] != -1){		
+			    newVal += oldAry[r - 1][c];
+			}
+		    }catch(ArrayIndexOutOfBoundsException e){}
+		    
+		    try{
+			if(oldAry[r][c + 1] != -1){
+			    newVal += oldAry[r][c + 1];
+			}
+		    }catch(ArrayIndexOutOfBoundsException e){}
+		    
+		    try{
+			if(oldAry[r][c - 1] != -1){			
+			    newVal += oldAry[r][c - 1];
+			}
+		    }catch(ArrayIndexOutOfBoundsException e){}
+		    
+		    
+		    newAry[r][c] = newVal;
+		}
+	    }
+	}
+	//printPastures();
+	//recursive call
+	//note the switching of newAry with oldAry
+	return navigateH(newAry, oldAry, seconds - 1);
+    }
+
+    //prints out pastures (for tests)
+    private void printPastures(){
+	for(int r = 0; r < pasture1.length; r++){
+	    for(int c = 0; c < pasture1[r].length; c++){
+		if(c == pasture1[r].length - 1){
+		    System.out.println(pasture1[r][c] + "," + pasture2[r][c] + "  ");
+		}
+		else{
+		    System.out.print(pasture1[r][c] + "," + pasture2[r][c] + "  ");
+		}
+	    }
+	}
+	System.out.println("\n");
+    }
+    
+    
+    //OLD METHOD (SLOW)
+    /* 
+    
     private char[][] pasture;
     private int numPaths; //final answer
-    
-    
     public int silver(String filename){
 	try{
 	    silverFile = new File(filename);
@@ -183,11 +301,17 @@ public class USACO{
 	navigateH(r1, c1 + 1, r2, c2, timeLeft - 1);
 	navigateH(r1, c1 - 1, r2, c2, timeLeft - 1);
     }
+    */
 		
     //TESTS
     public static void main(String[] args){
 	USACO m = new USACO();
-	//System.out.println(m.bronze("bronzetest1.txt"));
-	System.out.println(m.silver("silverTest2.txt"));
+	try{
+	    //System.out.println(m.bronze("bronzetest1.txt"));
+	    System.out.println(m.silver(args[0]));
+	}catch(ArrayIndexOutOfBoundsException e){
+	    System.out.println("Use args to test stuff!!!");
+	    System.exit(1);
+	}
     }
 }
