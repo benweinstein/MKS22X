f175a80824dcbd4d936999fcf33f122265e0061c
Ben Weinstein <bweinstein@stuy.edu>
2017-03-30 14:31:05 -0400

    Phase I completed (v2)

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -15,7 +15,7 @@ public class MyLinkedList{
     
     //adds the value to the start of a linked list	
     private boolean addToStart(int value){
-	LNode newStart = new LNode(value, start);
+	LNode newStart = new LNode(value);
 	start = newStart;
 	size++;
 	return true;
@@ -30,9 +30,8 @@ public class MyLinkedList{
 	else{
 	    LNode oldEnd = getNthNode(size - 1);
 	    oldEnd.next = new LNode(value); 
-	    //LNode oldEnd = new LNode(getNthNode(size - 1).value, newEnd);
 	}
-
+	
 	size++;
 	return true;
     }
@@ -44,20 +43,34 @@ public class MyLinkedList{
     
     //return the value of the element at the specified index (0 based)
     public int get(int index){
+	if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
 
+	return getNthNode(index).value;
     }
 
     //change the value of the element at the specified index to the newValue,
     //return the old value
     public int set(int index, int newValue){
+	if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
 
+	LNode beingReplaced = getNthNode(index);
+	int holder = beingReplaced.value;
+
+	beingReplaced.value = newValue;
+	    
+	return holder;
     }
 
     //helper method, super useful
     private LNode getNthNode(int n){
 	LNode nth = start;
 	
-	while(n > 0){
+	while(nth.next != null && n > 0){
+	    //System.out.println(nth.value);
 	    nth = nth.next;
 	    n--;
 	}
@@ -102,7 +115,7 @@ public class MyLinkedList{
 	//so you can create an end node
 	public LNode(int val){
 	    value = val;
-	    //next defaults to null
+	    next = null;
 	}
     }
 
@@ -114,5 +127,10 @@ public class MyLinkedList{
 	l.add(3);
 	l.add(5);
 	System.out.println(l);
+	//System.out.println(l.get(5));
+	System.out.println(l.set(2, 234)); //5
+	System.out.println(l.set(0, 66)); //34
+	System.out.println(l); //[ 66, 3, 234]
+
     }
 }
