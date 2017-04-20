6817199599a5e567067f93fbfc52c027e98273d7
Ben Weinstein <bweinstein@stuy.edu>
2017-04-20 17:17:04 -0400

    implemented pseudocode from class (v0)

---
+++ PostFix/PostFix.java
@@ -0,0 +1,55 @@
+import java.util.Stack;
+
+public class PostFix{
+    public static double eval(String s){
+	String[] tokens = s.split(" ");
+	Stack<String> values = new Stack<String>();
+
+	for(String token : tokens){
+	    if(isOperator(token)){
+		values.push(apply(token, values.pop(), values.pop()));
+	    }
+	    else{
+		values.push(token);
+	    }
+	}
+	
+	return Double.parseDouble(values.pop());
+    }
+
+    private static String apply(String operation, String a1, String b1){
+	double a = Double.parseDouble(a1);
+	double b = Double.parseDouble(b1);
+	char op = operation.charAt(0);
+
+	if(op == '+'){
+	    return a + b + "";
+	}
+	if(op == '-'){
+	    return b - a + ""; //order switches b/c pop() happens in reverse!!!
+	}
+	if(op == '*'){
+	    return a * b + "";
+	}
+	if(op == '/'){
+	    return b / a + "";
+	}
+	//last case is mod:
+	return b % a + "";
+    }
+
+    private static boolean isOperator(String s){
+	char a = s.charAt(0);
+	return (a == '+' ||
+		a == '-' ||
+		a == '*' ||
+		a == '/' ||
+		a == '%');
+    }
+
+    public static void main(String[] args){
+	System.out.println(eval("5 2 + 3 * 8 -")); //13
+	System.out.println(eval("0 1 2 3 * + +")); //7
+	
+    }
+}
