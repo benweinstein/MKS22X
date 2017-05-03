94ba6253091f31f77e3069244afecff12abf9c00
Ben Weinstein <bweinstein@stuy.edu>
2017-05-03 14:47:19 -0400

    code frame (v0)

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,36 @@
+public class MyHeap{
+    private int size;
+    private int minMaxConstant;//EITHER -1 or 1, ALWAYS
+
+    public MyHeap(){
+    
+    }
+
+    public MyHeap(boolean minMax){
+    
+    }
+
+    public String peek(){
+
+    }
+
+    public String remove(){
+
+    }
+
+    public void add(String s){
+
+    }
+
+    private void pushDown(){
+
+    }
+
+    private void pushUp(){
+
+    }
+
+    public static void main(String[] args){
+	System.out.println("Hi");
+    }
+}
