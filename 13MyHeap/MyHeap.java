7e6c760273104157d5a3753712b281b3074aea09
Ben Weinstein <bweinstein@stuy.edu>
2017-05-04 08:37:46 -0400

    needs more work (v1)

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,25 +1,39 @@
 public class MyHeap{
     private int size;
     private int minMaxConstant;//EITHER -1 or 1, ALWAYS
-
-    public MyHeap(){
+    private ArrayList<String> data;
     
+    public MyHeap(){
+	minMaxConstant = 1; //maxHeap
+	data = new ArrayList<String>();
+	data.add("DO NOT PRINT ME");
     }
 
-    public MyHeap(boolean minMax){
-    
+    //if true: maxHeap; if false: minHeap
+    public MyHeap(boolean minOrMax){
+	if(minOrMax){
+	    minMaxConstant = 1;
+	}
+	else{
+	    minMaxConstant = -1;
+	}
+
+	data = new ArrayList<String>();
+	data.add("DO NOT PRINT ME"); 
     }
 
     public String peek(){
-
+	return data.get(1);
     }
 
     public String remove(){
 
+	size--;
     }
 
     public void add(String s){
 
+	size++;
     }
 
     private void pushDown(){
@@ -29,7 +43,15 @@ public class MyHeap{
     private void pushUp(){
 
     }
-
+    
+    public String toString(){
+	String ans = "";
+	for(int i = 1; i < data.size(); i++){
+	    ans += data.get(i) + ", ";
+	}
+	return ans;
+    }
+    
     public static void main(String[] args){
 	System.out.println("Hi");
     }
