58c5de8bbc2c9a1eaa770c8311fc737d4633690e
Ben Weinstein <bweinstein@stuy.edu>
2017-05-15 14:43:31 -0400

    FrontierPriorityQueue frame

---
+++ 15MazeSolver/FrontierPriorityQueue.java
@@ -0,0 +1,15 @@
+public class FrontierPriorityQueue{
+    private PriorityQueue<Location> locations;
+
+    public void add(Location l){
+
+    }
+
+    public Location next(){
+
+    }
+
+    public static void main(String[] args){
+	System.out.println("Hi");
+    }
+}
